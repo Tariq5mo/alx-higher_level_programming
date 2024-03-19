49a46d47151b74184779368ed92e60906d69a9ce
Talal4024 <Talal4024@gmail.com>
2024-03-19 01:38:23 +0000

    Add task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     if not my_string:
         return None
-    str = my_string.translate({ ord("c"): None })
-    str = str.translate({ ord("C"): None })
+    str = my_string.translate({ord("c"): None})
+    str = str.translate({ord("C"): None})
     return str

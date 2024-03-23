6662dceadd6769794674b3535e49ae8844016aa7
Talal4024 <Talal4024@gmail.com>
2024-03-23 23:16:19 +0000

    Add task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    dic = dict(a_dictionary)
+    i = 0
+    for check in dic:
+        if dic[check] is not None:
+            i = 1
+            break
+    if i == 0:
+        return None
+    k = check
+    for key in dic:
+        if dic[key] is not None:
+            if dic[key] > dic[k]:
+                k = key
+    return k
--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    di = dict(a_dictionary)
+    for i in di:
+        di[i] = int(di[i]) * 2
+    return di

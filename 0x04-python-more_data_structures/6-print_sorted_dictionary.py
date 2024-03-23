7635b777c004ee6be389755470512285e070dc93
Talal4024 <Talal4024@gmail.com>
2024-03-23 15:51:27 +0000

    Add tasks 6 and 7

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    di = dict(a_dictionary)
+    order_di = sorted(di)
+    for key in order_di:
+        print("{}: {}".format(str(key), di[key]))

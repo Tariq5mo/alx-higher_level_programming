7a02e9eb9a716b2cc201ded242047ae0acb39c58
Talal4024 <Talal4024@gmail.com>
2024-04-02 17:45:00 +0000

    Add task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    if my_list is None or x is None:
+        return 0
+    n = 0
+    i = 0
+    rx = int(x)
+    rlist = list(my_list)
+    while (i < rx):
+        try:
+            print("{:d}".format(rlist[i]), end="")
+            n = n + 1
+            i = i + 1
+        except (ValueError, TypeError):
+            i = i + 1
+        except IndexError as e:
+            print()
+            return n
+    print("")
+    return n

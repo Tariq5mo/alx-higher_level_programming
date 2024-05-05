6ca9f6366ffb362cff1aaeec5544a18acf13ee8f
Tariq5mo <tariqomer756@gmail.com@gmail.com>
2024-05-05 16:57:31 +0100

    Add task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''Task 10.'''
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Define a square."""
+
+    def __init__(self, size):
+        """
+        size must be private. No getter or setter
+        size must be a positive integer, validated by integer_validator.
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        '''Return the area of square.'''
+        return self.__size ** 2
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)

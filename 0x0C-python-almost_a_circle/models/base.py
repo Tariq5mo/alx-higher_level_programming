bcf1e4f28597f751049c7617f188dd6c3bdca7cc
Tariq5mo <tariqomer756@gmail.com@gmail.com>
2024-05-19 12:16:36 +0100

    Fix task 1

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,7 +5,7 @@
 
 class Base:
     """This class will be the “base” of all other classes in this project.
-    """    
+    """
     __nb_objects = 0
 
     def __init__(self, id=None):
@@ -13,8 +13,8 @@ class Base:
 
         Args:
             id (int): The id of the instance. Defaults to None.
-        """        
-        if id == None:
+        """
+        if id is None:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
         else:

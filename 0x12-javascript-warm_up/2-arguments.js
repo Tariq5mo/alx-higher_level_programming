8e78a0515295ffabb355b669a9140df0e538230a
Tariq5mo <tariqomer756@gmail.com>
2024-07-27 17:17:52 +0300

    Add tasks 3, 4, 5 and 6

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,7 +2,6 @@
 /* Prints a message depending of the number of arguments passed */
 const args = process.argv;
 const lenArgs = args.length;
-console.log(lenArgs);
 if (lenArgs <= 2) {
   console.log('No argument');
 } else if (lenArgs === 3) {
---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/*
+Prints 3 lines: (like 1-multi_languages.js)
+but by using an array of string and a loop
+*/
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let index = 0; index < arr.length; index++) {
+  console.log(arr[index]);
+}

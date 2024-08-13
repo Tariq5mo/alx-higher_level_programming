6a3f098f3aeda89d320f15131c30010a95a618de
Tariq5mo <tariqomer756@gmail.com>
2024-08-13 09:03:48 +0300

    Add task 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+/* Script that display the status code of a GET request. */
+const request = require('request');
+
+request.get(process.argv[2], (err, res, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    let i = 0;
+    const ti = JSON.parse(body.toString());
+    for (let index = 0; index < ti.results.length; index++) {
+      for (
+        let ind = 0;
+        ind < ti.results[index].characters.length;
+        ind++
+      ) {
+        if (
+          ti.results[index].characters[ind] ===
+          'https://swapi-api.alx-tools.com/api/people/18/'
+        ) {
+          i += 1;
+        }
+      }
+    }
+    console.log(i);
+  }
+});

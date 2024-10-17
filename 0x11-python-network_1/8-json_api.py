9885593b5e12aff72c258feccd2fed04cd68892c
Tariq5mo <tariqomer756@gmail.com>
2024-10-17 14:52:20 +0300

    Fix

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -2,8 +2,8 @@
 """takes in a letter and sends a POST request to
 http://0.0.0.0:5000/search_user with the letter as a parameter.
 """
+import sys, requests
 if __name__ == "__main__":
-    import sys, requests
     if len(sys.argv) < 2:
         q = ""
     else:

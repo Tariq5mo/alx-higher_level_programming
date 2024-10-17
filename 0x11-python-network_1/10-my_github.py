7f4ac35b9a22da8158133254550bcc4a1e8b0353
Tariq5mo <tariqomer756@gmail.com>
2024-10-17 15:44:45 +0300

    Fix task 10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -5,5 +5,5 @@ uses the GitHub API to display your id
 from requests import get
 from sys import argv
 if __name__ == "__main__":
-	response = get("https://api.github.com/user", auth=(argv[1], argv[2]))
-	print(response.json().get("id"))
+    response = get("https://api.github.com/user", auth=(argv[1], argv[2]))
+    print(response.json().get("id"))
